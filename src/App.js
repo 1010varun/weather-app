1768fc4d5ea06eae0d2e929af68e3ded58d31834
1010varun <varunagarwal10.2@gmail.com>
2022-12-02 00:49:53 +0530

    add: added responsiveness

--- src/App.js
+++ src/App.js
@@ -1,84 +1,137 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
-import { Button, Input, Alert } from "reactstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "./App.css";
-import Navbarsite from "./components/Navbar";
-import { Card, CardBody} from "reactstrap";
-import {IoIosWater} from "react-icons/io"
-import {BsWind, BsThermometerHalf} from "react-icons/bs"
-import {BiMap} from "react-icons/bi"
-import {FaSun} from "react-icons/fa"
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { IoIosWater } from "react-icons/io";
+import { BsWind, BsThermometerHalf } from "react-icons/bs";
+import { BiMap } from "react-icons/bi";
+import { FaSun } from "react-icons/fa";
+import {toast, ToastContainer} from "react-toastify"
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-
-  const[latitude, setLatitude] = useState('');
-  const[longitude, setLongitude] = useState('');
-  const[city, setCity] = useState('');
-  const[country, setCountry] = useState('');
-  const[datas, setDatas] = useState({});
-  const[temp, setTemp] = useState(0);
-  const[humidity, setHumidity] = useState(0)
-  const[description, setDescription] = useState(0)
-  const[name, setName] = useState(0)
-  const[windspeed, setWindspeed] = useState(0)
-  
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
+  const [datas, setDatas] = useState({});
+  const [temp, setTemp] = useState(0);
+  const [humidity, setHumidity] = useState(0);
+  const [description, setDescription] = useState(0);
+  const [name, setName] = useState(0);
+  const [windspeed, setWindspeed] = useState(0);
 
   const callbylatlon = async () => {
-    const { data } = await Axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=a9553eeffc4cfe23a2011d3fb64edc72`)
-    setDatas(data);
-  }
+    if(latitude.length !== 0 && longitude.length !== 0){
+      await Axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=a9553eeffc4cfe23a2011d3fb64edc72`
+      )
+        .then(({ data }) => {
+          toast.success("SUCCESSFULLY Fetched weather", { theme: "dark" });
+          setDatas(data);
+        })
+        .catch(() => {
+          toast.error("ERROR, Please check & try again", { theme: "dark" });
+        });
+    } else{
+      return toast.warn("Please Give location premission", { theme: "dark" });
+    }
+  };
 
   const callbyname = async () => {
-    if(city !== 0 && country !== 0){
-      const {data} = await Axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=a9553eeffc4cfe23a2011d3fb64edc72`)
-      setDatas(data);
+    if (city.length !== 0 && country.length !== 0) {
+      await Axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=a9553eeffc4cfe23a2011d3fb64edc72`
+      ).then(({data}) => {
+        toast.success("SUCCESSFULLY Fetched weather", {theme: 'dark'})
+        setDatas(data);
+      }).catch(() => {
+        toast.error("ERROR, Please check & try again", { theme: "dark" });
+      })
+    } else{
+      return toast.warn("Please Enter Details !!",{theme: "dark"});
     }
-  }
+  };
 
   const location = () => {
     navigator.geolocation.getCurrentPosition((position) => {
       setLatitude(position.coords.latitude);
-      setLongitude(position.coords.longitude);  
-    })
-  }
+      setLongitude(position.coords.longitude);
+    });
+  };
 
   useEffect(() => {
     location();
   }, []);
 
   useEffect(() => {
-    if(Object.keys(datas).length > 0){
-      setTemp(Math.ceil((datas.main?.temp)-273));
+    if (Object.keys(datas).length > 0) {
+      setTemp(Math.ceil(datas.main?.temp - 273));
       setHumidity(datas.main?.humidity);
       setName(datas.name);
       setDescription(datas.weather[0].description);
       setWindspeed(datas.wind?.speed);
     }
-  }, [datas])
-  return(
+  }, [datas]);
+  return (
     <div>
-      <Navbarsite/>
-      <Input type="text" placeholder="Enter City Name" value={city} className="city"
-      onChange={e => {setCity(e.target.value)}}/>
-      <Input type="text" placeholder="Enter Country Name" value={country} className="country"
-      onChange={e => {setCountry(e.target.value)}}/><br></br>
-      <Button color="success" onClick={callbyname} className="button">Get Tru Weather By City Name</Button>
-      <Button color="primary" onClick={callbylatlon} className="button2">By Current Location</Button>
-      <Card className="cards">
-        <CardBody className="text-center mt-5">
-        <div><BsThermometerHalf size={20}/> {temp} °C</div>
-        <div><IoIosWater size={20}/> {humidity} %</div>
-        <div><FaSun/> {description}</div>
-        <div><BiMap/> {name}</div>
-        <div><BsWind/> {windspeed}Km/h</div>
-        </CardBody>
-      </Card>
+      <div className="bg-black text-white p-3 lg:text-xl"> tru Weather</div>
+      <div className="d-flex flex-column gap-2 mt-5 mx-2">
+        <input
+          type="text"
+          placeholder="Enter City Name"
+          value={city}
+          className="w-10/12 p-2 rounded-lg mx-auto sm:w-8/12 md:w-6/12 lg:w-5/12"
+          onChange={(e) => {
+            setCity(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Enter Country Name"
+          value={country}
+          className="w-10/12 p-2 rounded-lg mx-auto sm:w-8/12 md:w-6/12 lg:w-5/12"
+          onChange={(e) => {
+            setCountry(e.target.value);
+          }}
+        />
+      </div>
+      <div className="mt-4 mx-2 d-flex flex-column">
+        <button
+          type="button"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 text-sm rounded-lg px-5 py-2.5 mb-2 focus:outline-none mx-auto"
+          onClick={callbyname}
+        >
+          Get Tru Weather By City Name
+        </button>
+        <button
+          type="button"
+          className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-blue-300 text-sm rounded-lg px-5 py-2.5 mx-auto mb-2 focus:outline-none"
+          onClick={callbylatlon}
+        >
+          Get Tru Weather By Current Location
+        </button>
+      </div>
+
+      <div className="block w-9/12 p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 mx-auto mt-5 d-flex flex-column justify-center items-center sm:w-6/12 md:w-4/12 lg:w-3/12 mb-5">
+        <p className="d-flex flex-row">
+          <BsThermometerHalf size={20} className="me-3" /> {temp} °C
+        </p>
+        <p className="d-flex flex-row">
+          <IoIosWater size={20} className="me-3" /> {humidity} %
+        </p>
+        <p className="d-flex flex-row">
+          <FaSun className="me-3" size={20} /> {description}
+        </p>
+        <p className="d-flex flex-row">
+          <BiMap className="me-3" size={20} /> {name}
+        </p>
+        <p className="d-flex flex-row">
+          <BsWind className="me-3" size={20} /> {windspeed}Km/h
+        </p>
+      </div>
+      <ToastContainer/>
     </div>
-  )
-
-}
-
+  );
+};
 
 export default App;
