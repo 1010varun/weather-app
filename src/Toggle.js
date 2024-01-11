54a7d5c60442c5533d4c1b687d1bcdf0edfbb7f8
Akshat Verma <112319520+Akshat162001@users.noreply.github.com>
2024-01-11 23:40:14 +0530

    "Contains new feature in toggle.js"

--- src/Toggle.js
+++ src/Toggle.js
@@ -3,12 +3,10 @@ import { FaToggleOff } from "react-icons/fa6";
 import { FaToggleOn } from "react-icons/fa6";
 const Toggle = ({ onToggle }) => {
   const [isDarkMode, setIsDarkMode] = useState(true);
-
   const handleToggle = () => {
     setIsDarkMode(!isDarkMode);
     onToggle(!isDarkMode);
   };
-
   return (
     <div className="toggle-container" style={{marginLeft:'1220px'}}>
       {isDarkMode ? (
